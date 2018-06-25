587917c58d21b7ae03b0a7afd12ef45aeef71c7f
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-25 18:46:32 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,12 @@
 # code here!
 class School
+roster = {}
   def initialize(name)
     @name = name
-    roster = {}
   end
+
   def add_student(name,grade)
     roster[grade] = []
     roster[grade] << name
   end
 end
-
-  
